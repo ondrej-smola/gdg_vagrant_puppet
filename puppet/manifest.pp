ff7b3a74bd8c57a3644cfb52afbd357a9a5731d1
Ondrej Smola <ondrej.smola@gmail.com>
2013-12-04 19:24:56 +0100

    cleanup

--- puppet/manifest.pp
+++ puppet/manifest.pp
@@ -1,7 +1,6 @@
 
 $application_name = 'app1'
 
-
 $applications_root = '/usr/local/applications'
 $apache_home = '/etc/apache2'
 
@@ -9,20 +8,16 @@ $application_home = "$applications_root/$application_name"
 $apache_sites_available = "$apache_home/sites-available" 
 $apache_sites_enabled = "$apache_home/sites-enabled"
 
-package{"apache2":
+package{ "apache2":
 	ensure => latest	
 }
 
-service{"apache2":
+service{ "apache2":
 	ensure => running,
 	enable => true,					
 	require => Package["apache2"]  	
 }
 
-user { 'www-data':
-	ensure => present
-}
-->
 file{ $applications_root:
 	ensure => directory,	
 	mode	=> 755,
@@ -37,20 +32,20 @@ file{ $application_home:
 	source => "/vagrant/$application_name"		
 }
 ->
-file{"$apache_sites_available":
-	 ensure => directory,
-	 recurse => true,
-	 purge => true
+file{ $apache_sites_available:
+	ensure => directory,
+	recurse => true,
+	purge => true
 }
 ->
-file{"$apache_sites_available/${application_name}":	
+file{ "$apache_sites_available/${application_name}":	
 	ensure => 'file',
 	owner => 'www-data',
 	mode => '755',	
 	content => template('/vagrant/puppet/templates/apache_instance.erb')		
 }
 ->
-file{$apache_sites_enabled:
+file{ $apache_sites_enabled:
 	recurse => true,
 	purge => true,
 	source => $apache_sites_available,
